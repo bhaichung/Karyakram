2e54ef7f15455494dd500787e374d71b2146d66c
Asmit11 <sandeepgahatraj9@gmail.com>
2022-12-03 10:45:25 +0545

    edited

--- src/view/userDashboard.java
+++ src/view/userDashboard.java
@@ -42,7 +42,7 @@ public class userDashboard extends javax.swing.JFrame {
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setPreferredSize(new java.awt.Dimension(1366, 768));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
