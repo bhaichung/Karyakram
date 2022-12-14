2f2a0567e589249b45b9e1d9c3b8d972a6f9cd9a
AbhigyaShrestha2060 <abhigyashrestha730@gmail.com>
2022-12-14 12:48:52 +0545

    edited in about about_us

--- src/view/about_us.java
+++ src/view/about_us.java
@@ -15,6 +15,8 @@ public class about_us extends javax.swing.JFrame {
      */
     public about_us() {
         initComponents();
+        
+        
     }
 
     /**
@@ -36,8 +38,9 @@ public class about_us extends javax.swing.JFrame {
         jButton7 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        aboutus = new javax.swing.JTextArea();
         jPanel2 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -95,35 +98,37 @@ public class about_us extends javax.swing.JFrame {
         jLabel1.setText("About Us");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 160, 180, -1));
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jTextArea1.setRows(5);
-        jTextArea1.setText("Karyakram is an event management application that helps in managing the most prestigious events the \nholds around the country. we provide different types of events varying from professional events to \nlifestyle based. Not only being able to hold larger events we also help out in holding small events such \nas co-operate Events, Meetings, ConferencesLive, Formal dinners and many other. It specializes in helping\nworkplace & organization unify their built environments for better management of people, places, and \nresources. This is our first step to making our event secured and successful.");
-        jTextArea1.setBorder(null);
-        jScrollPane1.setViewportView(jTextArea1);
+        aboutus.setColumns(20);
+        aboutus.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        aboutus.setRows(5);
+        aboutus.setText("Karyakram is an event management application that helps in\nmanaging the most prestigious events the holds around the \ncountry. we provide different types of events varying from \nprofessional events to lifestyle based. Not only being able to \nhold larger events we also help out in holding small events\nsuch as co-operate Events, Meetings, ConferencesLive, Formal \ndinners and many other. It specializes in helping workplace & \norganization unify their built environments for better \nmanagement of people, places, and resources.This is our first \nstep to making our event secured and successful.");
+        aboutus.setBorder(null);
+        jScrollPane1.setViewportView(aboutus);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 260, 1200, 220));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 720, 380));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1370, Short.MAX_VALUE)
+            .addGap(0, 100, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 760, Short.MAX_VALUE)
+            .addGap(0, 100, Short.MAX_VALUE)
         );
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1370, 760));
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/bg.png"))); // NOI18N
+        jLabel4.setText("jLabel4");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 260, 560, 440));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -181,6 +186,7 @@ public class about_us extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea aboutus;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
@@ -189,9 +195,9 @@ public class about_us extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
 }
